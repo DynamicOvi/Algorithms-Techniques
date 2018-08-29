1768b4791daf48f373e7a5cd147a8600a52d3289
Mohammad Hafiz-Al-Masud <dynamicovi@gmail.com>
2018-08-30 02:04:38 +0600

    Create Binary to Decimal.cpp

---
+++ Binary to Decimal.cpp
@@ -0,0 +1,17 @@
+//Binary(string) to Decimal(long long)
+
+ll b2d(string s)
+{
+    reverse(s.begin(),s.end());
+    ll n=0;
+    ll i,j;
+    for(i=0; i<s.size(); i++)
+    {
+        ll tmp=s[i]-'0';
+        ll r=i;
+        while(r--)
+            tmp*=2;
+        n+=tmp;
+    }
+    return n;
+}
